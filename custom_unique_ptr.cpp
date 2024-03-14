a758d0b4476c8d68c39721c49b21806f961e11ae
rathodabhishek36 <arathod@cs.iitr.ac.in>
2024-03-14 13:12:01 +0530

    custom unique ptr - support runtime polymorphism

--- custom_unique_ptr.cpp
+++ custom_unique_ptr.cpp
@@ -1,5 +1,4 @@
 #include <cassert>
-#include <cstdio>
 #include <fstream>
 #include <iostream>
 #include <memory>
@@ -15,10 +14,16 @@ struct custom_unique_ptr {
     using deleter_type = Deleter;
 
     custom_unique_ptr() : mptr_(nullptr) {}
+    
     custom_unique_ptr(pointer_type p) : mptr_(p) { p=nullptr; }
-    custom_unique_ptr(pointer_type p, deleter_type&& d) : mptr_(p), dlt_(std::move(d)) { p=nullptr; }
+    custom_unique_ptr(pointer_type p, deleter_type&& d) : mptr_(p), mdlt_(std::move(d)) { p=nullptr; }
+
     custom_unique_ptr(const custom_unique_ptr&) = delete;
-    custom_unique_ptr(custom_unique_ptr&& p) : mptr_(std::move(p.mptr_)) {
+    custom_unique_ptr(custom_unique_ptr&& p) : mptr_(p.mptr_), mdlt_(std::move(p.mdlt_)) {
+        p.mptr_=nullptr;
+    }
+    template<typename T1, typename D1>
+    custom_unique_ptr(custom_unique_ptr<T1, D1>&& p) : mptr_(p.mptr_), mdlt_(std::move(p.mdlt_)) { 
         p.mptr_=nullptr;
     }
 
@@ -30,6 +35,13 @@ struct custom_unique_ptr {
     
     custom_unique_ptr& operator=(custom_unique_ptr&& p) {
         reset(p.release());
+        return *this;
+    }
+
+    template<typename T1, typename D1>
+    custom_unique_ptr& operator=(custom_unique_ptr<T1, D1>&& p) {
+        reset(p.release());
+        mdlt_ = std::move(p.get_deleter());
         return *this;
     }
 
@@ -61,13 +73,13 @@ struct custom_unique_ptr {
         return mptr_;
     }
 
-    Deleter get_deleter() noexcept {
-        return dlt_;
+    deleter_type& get_deleter() noexcept {
+        return mdlt_;
     }
 
 private:
     pointer_type mptr_;
-    Deleter dlt_;
+    deleter_type mdlt_;
 };
 
 namespace ar {
@@ -113,7 +125,15 @@ int main() {
         // p is now in a moved-from 'empty' state, equal to nullptr
         assert(!p);
     }
-    std::cout << "\n" "2) Custom deleter demo\n";
+     std::cout << "\n" "2) Runtime polymorphism demo\n";
+    {
+        // Create a derived resource and point to it via base type
+        std::unique_ptr<B> p = std::make_unique<D>();
+ 
+        // Dynamic dispatch works as expected
+        p->bar();
+    }
+    std::cout << "\n" "3) Custom deleter demo\n";
     std::ofstream("demo.txt") << 'x'; // prepare the file to read
     {
         using unique_file_t = custom_unique_ptr<std::FILE, decltype(&close_file)>;
@@ -122,7 +142,7 @@ int main() {
             std::cout << char(std::fgetc(fp.get())) << '\n';
     } // `close_file()` called here (if `fp` is not null)
  
-    std::cout << "\n" "3) Custom lambda-expression deleter and exception safety demo\n";
+    std::cout << "\n" "4) Custom lambda-expression deleter and exception safety demo\n";
     try
     {
         std::unique_ptr<D, void(*)(D*)> p(new D, [](D* ptr)
